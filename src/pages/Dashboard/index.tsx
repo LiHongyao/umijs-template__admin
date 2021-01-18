0802f3497f3979b2bcbe5de2cb2fde3a6d269144
lihongyao <lihy_online@163.com>
2021-01-18 15:25:26 +0800

    优化

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,9 @@
 import React, { FC, useEffect, useRef } from 'react';
 // 引入echarts
-import echarts from 'echarts/lib/echarts';
+import * as echarts from 'echarts/lib/echarts';
 // 引入柱状图
 import 'echarts/lib/chart/bar';
+import 'echarts/lib/chart/line';
 // 引入提示框和标题组件
 import 'echarts/lib/component/tooltip';
 import 'echarts/lib/component/title';
@@ -13,27 +14,30 @@ const Dashboard: FC = () => {
   useEffect(() => {
     if (echartsRef.current) {
       const myChart = echarts.init(echartsRef.current);
-      myChart.setOption({
+      // 指定图表的配置项和数据
+      var option = {
         title: {
-          text: '模拟数据',
-          subtext: '仅供参考',
+          text: 'ECharts 入门示例',
         },
         tooltip: {},
         legend: {
-          data: ['日活'],
+          data: ['销量'],
         },
         xAxis: {
-          data: ['DDOU', 'D迅', '数字通', 'DCEP'],
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子'],
         },
         yAxis: {},
         series: [
           {
-            name: '日活',
+            name: '销量',
             type: 'bar',
-            data: [5, 20, 36, 10],
+            data: [5, 20, 36, 10, 10, 20],
           },
         ],
-      });
+      };
+
+      // 使用刚指定的配置项和数据显示图表。
+      myChart.setOption(option);
     }
   }, [echartsRef]);
 
